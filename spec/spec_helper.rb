f68aa3a3a1e12b0b4b29684265892c863a87513f
Andrew Radev <andrey.radev@gmail.com>
2022-04-03 17:00:12 +0300

    Activate timecop's safe mode, just in case

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  Timecop.safe_mode = true
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
