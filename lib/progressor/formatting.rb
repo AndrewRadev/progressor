558f50ebb11882ea3e02c77d64f2afd4d7913072
Andrew Radev <andrey.radev@gmail.com>
2024-08-04 11:53:28 +0200

    No need for .round(2)
    We're truncating the number when printing anyway.

--- lib/progressor/formatting.rb
+++ lib/progressor/formatting.rb
@@ -16,12 +16,12 @@ class Progressor
         "#{format_float(time.round(2))}s"
       elsif time < 3600
         minutes = time.to_i / 60
-        seconds = (time - minutes * 60).round(2)
+        seconds = time - minutes * 60
         "#{format_int(minutes)}m:#{format_int(seconds)}s"
       else
         hours = time.to_i / 3600
         minutes = (time.to_i % 3600) / 60
-        seconds = (time - (hours * 3600 + minutes * 60)).round(2)
+        seconds = time - (hours * 3600 + minutes * 60)
         "#{format_int(hours)}h:#{format_int(minutes)}m:#{format_int(seconds)}s"
       end
     end
